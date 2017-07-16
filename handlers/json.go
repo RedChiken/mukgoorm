31175256a217b4ae7a56d7e06d237105428578e5
redchiken <gombabo1217@gmail.com>
2017-07-16 17:01:23 +0900

    add jstree but cannot distinguish file and folder

---
+++ handlers/json.go
@@ -0,0 +1,89 @@
+package handlers
+
+import(
+	"net/http"
+	"os"
+	//"encoding/json"
+	"io/ioutil"
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"github.com/zeropage/mukgoorm/setting"
+)
+
+func Json(c *gin.Context){
+	dir := setting.GetDirectory().Path
+
+	file , err1 := os.Stat(dir)
+	if err1 != nil {
+		log.Error(err1)
+		c.HTML(http.StatusNotFound, "error/404.tmpl", gin.H{})
+	}
+
+	ret, err2 := MakeJsTree(file, dir)
+	fmt.Print("\n\n")
+	if err2 != nil {
+		log.Error(err1)
+		c.HTML(http.StatusNotFound, "error/404.tmpl", gin.H{})
+	}
+
+	// jstreeFormat := map[string]map[string]JsTreeContainer{
+	// 	"core": map[string]JsTreeContainer{
+	// 		"data": ret,
+	// 	},
+	// }
+
+	jstreeFormat := map[string]JsTreeContainer{
+		"data": ret, 
+	}
+	fmt.Print(jstreeFormat)
+	fmt.Print("\n\n")
+
+	c.JSON(http.StatusOK, gin.H{
+		"core": jstreeFormat,
+	})
+}
+
+func MakeJsTree(rootFile os.FileInfo, rootDir string)(JsTreeContainer, error){
+	var subtree JsTreeContainer
+	if rootFile.IsDir() {	// i think this wasn't work correctly
+		files, err := ioutil.ReadDir(rootDir)
+		if err != nil {
+			panic(err)
+		} else {
+			var subByte []JsTreeContainer
+			for i := 0; i < len(files); i++ {
+				subdata, _ := MakeJsTree(files[i], rootDir + "/" + files[i].Name())
+				subByte = append(subByte, subdata)
+
+				fmt.Print("\n\n")
+				fmt.Print("this is folder : ")
+				fmt.Print(subByte)
+
+			}
+			subtree = JsTreeContainer{rootFile.Name(), subByte}
+
+			fmt.Print("\n")
+			fmt.Print(subtree)
+
+		}
+	} else {
+		subtree = JsTreeContainer{rootFile.Name(), []JsTreeContainer{}}
+
+		fmt.Print("\n")
+		fmt.Print("this is file : ")
+		fmt.Print(subtree)
+	}
+
+	fmt.Print("\n")
+	fmt.Print("now subtree : ")
+	fmt.Print(subtree)
+
+	return subtree, nil
+}
+
+type JsTreeContainer struct{
+	text string
+	Children []JsTreeContainer
+}
